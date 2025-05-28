cdadd538a6dfee3abb59a6078c67c76c1e850959
nngu0110 <nngu0110@uni.sydney.edu.au>
2025-05-29 00:30:38 +1000

    adjust pagetsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <h1 className="text-white italic">Hello</h1>
-  );
+ return (
+  <>
+   <nav className="bg-gray-800 p-4">
+    <ul className="flex space-x-4">
+     <li>
+      <Link className="text-white hover:text-gray-400" href="/dashboard">
+       Dashboard
+      </Link>
+     </li>
+     <li>
+      <Link className="text-white hover:text-gray-400" href="/building">
+       Building
+      </Link>
+     </li>
+     <li>
+      <Link className="text-white hover:text-gray-400" href="/customer">
+       Customer
+      </Link>
+     </li>
+     <li>
+      <Link className="text-white hover:text-gray-400" href="/pricing">
+       Pricing
+      </Link>
+     </li>
+    </ul>
+   </nav>
+  </>
+ );
 }
-``
